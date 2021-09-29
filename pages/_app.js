687cd2508ad4cf0149ca62991a06896d596e56fa
Randy AP <rrandy.kun@gmail.com>
2021-09-30 05:59:35 +0700

    konversi landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import "../styles/main.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* AOS Animation */}
+        <link
+          href="https://unpkg.com/aos@2.3.1/dist/aos.css"
+          rel="stylesheet"
+        />
+
+        {/* Bootstrap JS */}
+        <script src="/scripts/vendor/jquery/jquery.slim.min.js"></script>
+        <script src="/scripts/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+        {/* Navbar Scroll JS */}
+        <script src="/scripts/navbar-scroll.js"></script>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
